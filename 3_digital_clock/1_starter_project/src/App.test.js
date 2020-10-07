a29a91aff56a062634a635f3d78ab2350da9e2f4
codeclassifiers <codeclassifiers@gmail.com>
2020-10-07 22:43:21 +0530

    Removed project folders

--- 3_digital_clock/1_starter_project/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders digital clock text', () => {
-  const { getByText } = render(<App />);
-  const textElement = getByText(/Digital Clock/i);
-  expect(textElement).toBeInTheDocument();
-});
